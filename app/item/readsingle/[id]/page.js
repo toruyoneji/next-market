abf4e2dd339fd87db2a866dfb83ac0bf2ce643a0
toruyoneji <toruyoneji0514@icloud.com>
2025-04-22 10:03:10 +0900

    4/22 ...

--- app/item/readsingle/[id]/page.js
+++ app/item/readsingle/[id]/page.js
@@ -18,7 +18,7 @@ const ReadsingleItem = async(context) => {
    // console.log(singleItem);
     return(
         <>
-        <div>
+        <div className="grid-container-si">
             <div>
                 <Image src={singleItem.image} width={750} height={500} 
                 alt="item-image" priority />
